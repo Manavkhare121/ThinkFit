01c6f4c9226f2bdb58f4552483ecf0dbb310253a
Manav Khare <manavkhare121@gmail.com>
2026-01-23 17:28:41 +0530

    Set up backend structure with server, database connection, and environment configuration

---
+++ Backend/src/server.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv"
+import app from "./app.js"
+import connectDB from "./db/db.js"
+dotenv.config({
+    path:"./.env"
+})
+
+connectDB().then(()=>{
+    app.listen(process.env.PORT || 3000,()=>{
+        console.log(`server is running at port:${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+        console.log("MONGODB coneection is faild !!!",error)
+    })
