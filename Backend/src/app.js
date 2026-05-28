dc7664e5e55c915e9133f79c36ebdfb1f5c03c3d
Manav Khare <manavkhare121@gmail.com>
2026-05-28 18:04:47 +0530

    refactor: improve code formatting and organization in app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,28 +1,43 @@
-import express from "express"
-import cookieParser from "cookie-parser" 
-import cors from "cors"
-const app=express()
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
-app.use(cookieParser())
-app.use(cors({
-  origin: "http://localhost:5173", 
-  credentials: true
-}));
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
+const app = express();
 
-import userRouter from './routes/user.routes.js'
-app.use("/api/users",userRouter)
+app.use(express.json({ limit: "16kb" }));
 
-import CounsellorRoutes from './routes/counsellor.routes.js'
-app.use("/api/counsellor",CounsellorRoutes)
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb",
+  })
+);
 
-import adminRoutes from './routes/admin.routes.js'
-app.use("/api/admin",adminRoutes);
+app.use(cookieParser());
 
-import chatroutes from './routes/chat.routes.js'
-app.use('/api/chat',chatroutes)
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://think-fit-vert.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 
-import bookingRoutes from './routes/booking.routes.js'
-app.use('/api/booking',bookingRoutes)
+import userRouter from "./routes/user.routes.js";
+app.use("/api/users", userRouter);
+
+import CounsellorRoutes from "./routes/counsellor.routes.js";
+app.use("/api/counsellor", CounsellorRoutes);
+
+import adminRoutes from "./routes/admin.routes.js";
+app.use("/api/admin", adminRoutes);
+
+import chatroutes from "./routes/chat.routes.js";
+app.use("/api/chat", chatroutes);
+
+import bookingRoutes from "./routes/booking.routes.js";
+app.use("/api/booking", bookingRoutes);
+
 export default app;
